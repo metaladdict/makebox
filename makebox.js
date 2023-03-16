bbe4640c7e727b45f8045ca4dbb0a0a66ba175da
Erwan K <gitlab@wdev.pro>
2023-03-16 03:23:06 +0100

    nettoyage/rangement du code

--- makebox.js
+++ makebox.js
@@ -46,34 +46,9 @@
 		}
 
 
-		p.uploadConf = function()
-		{
-			console.log('Charger une configuration');
-			return false;
-		}
-		p.downloadConf = function()
-		{
-			console.log('Télécharger une configuration');
-			return false;
-		}
-		p.downloadSVG = function()
-		{
-			console.log('Téléchaerger le SVG');
-			var element = document.createElement('a');
-			text = document.querySelector('svg').outerHTML;
-			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + text);
-			element.setAttribute('download', 'plan.svg');
-		
-			element.style.display = 'none';
-			document.body.appendChild(element);
-		
-			element.click();
-		
-			document.body.removeChild(element);
-			return false;
-		}
-
-
+		/**********************************************************/
+		/*													 SVG													*/
+		/**********************************************************/
 		p.updateSVGView = function()
 		{
 			$('#preview *').remove();
@@ -114,7 +89,7 @@
 			for(cote=0; cote<4; cote++)
 			{
 				var obj = {
-					w: 			(p.isEven(cote) ? dts.w : dts.h), 
+					w: 				p.isEven(cote) ? dts.w : dts.h, 
 					sens:			cote, 
 					type:			dts.cotes[cote], 
 					sides:		[
@@ -141,87 +116,88 @@
 			document.getElementById("preview").innerHTML += "";
 		}
 
-		p.creerElement = function(req)
+		p.creerLigneDecoupe = function(req)
 		{
-			var def = {
-				'type': 'quad', 'x' : 0, 'y' : 0, 'w' : 75, 'h' : 40, 
-				'cotes':	"1111", 'dlta': [0,0,0,0], "id" : 'elm'+p.uid(), 'kp':[0,0,0,0,0,0,0,0]
+			def = {w: 50, sens: 0, type:1, delta: 0, sides: [0,0]};
+			var cnf = $.extend({}, def, req);
+			//console.log(cnf);
+			cmd = [];
+
+			// sélection de la progression de découpe : horizontal/vertical, xy/yx et direction
+			cld = p.isEven(cnf.sens) ? 'hv' : 'vh';
+			dir = p.isEven(cnf.type) ? -1 : 1;
+			mlt = {
+				x: (cnf.sens<2) ? 1 : -1,
+				y: (cnf.sens==1 || cnf.sens==2) ? -1 : 1
 			};
-			var blockConf = $.extend({}, def, req);
-			blockConf.coins = p.recreerCoins(blockConf.cotes);
-			var zconf = $('<div class="zconf"></div>');
-			var btnlst = $('<div class="btnlst"></div>').appendTo(zconf);
-			$('<a href="#">X</a>').click(p.killMe).appendTo(btnlst);
-			$('<a href="#">+1</a>').click(p.copyMe).appendTo(btnlst);
-
-			p.creerFieldSet('base',JSON.parse('[["w","Larg.", '+blockConf.w+'],["h","Haut.", '+blockConf.h+'],["x","X", '+blockConf.x+'],["y","Y", '+blockConf.y+']]')).addClass('quadri').appendTo(zconf);
-			p.creerFieldSet('dlta',JSON.parse('[["dlta.0","⮝", '+blockConf.dlta[0]+'],["dlta.1","⮞", '+blockConf.dlta[1]+'],["dlta.2","⮟", '+blockConf.dlta[2]+'],["dlta.3","⮜", '+blockConf.dlta[3]+']]')).addClass('quadri').prepend('<strong>Décalage</strong>').appendTo(zconf);
-			p.creerFieldSet('dkp1',JSON.parse('[["kp.0","⮝", '+blockConf.kp[0]+'],["kp.1","⮞", '+blockConf.kp[1]+'],["kp.2","⮟", '+blockConf.kp[2]+'],["kp.3","⮜", '+blockConf.kp[3]+'],["kp.4","⮝", '+blockConf.kp[0]+'],["kp.5","⮞", '+blockConf.kp[1]+'],["kp.6","⮟", '+blockConf.kp[2]+'],["kp.7","⮜", '+blockConf.kp[3]+']]')).addClass('quadri').prepend('<strong>Découpe delta / arrondi</strong>').appendTo(zconf);
 
 
-			var configurateur = $('<div class="element" data-cotes="'+blockConf.cotes+'" id="'+blockConf.id+'"></div>');
-			$('<div data-type="'+blockConf.coins[0]+'" class="coin hg"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.cotes[0]+'" class="cote h"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.coins[1]+'" class="coin hd"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.cotes[3]+'" class="cote g"><span></span></div>').appendTo(configurateur);
-			zconf.appendTo(configurateur);
-			$('<div data-type="'+blockConf.cotes[1]+'" class="cote d"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.coins[3]+'" class="coin bg"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.cotes[2]+'" class="cote b"><span></span></div>').appendTo(configurateur);
-			$('<div data-type="'+blockConf.coins[2]+'" class="coin bd"><span></span></div>').appendTo(configurateur);
-			
-			p.set.listElms[blockConf.id] = blockConf;
-			p.set.zoneElms.append(configurateur);
-
-			p.updateSVGView();
-		}
-
-		p.recreerCoins = function(c)
-		{
-			coins = "";
-
-			for(i=0; i<=3; i++)
+			if(cnf.type=="5")
 			{
-				duo = ((i==0)?c[3]:c[i-1])+''+c[i]; //création d'une chaine des cote précédents et suivants
-				if(duo=='11' || duo=='13' || duo=='41' || duo=='43')
-				{coins+='1';}
-				else if(duo=='51' || duo=='53' || duo=='21' || duo=='23' || duo=='31' || duo=='33')
-				{coins+='2';}
-				else if(duo=='52' || duo=='54' || duo=='55' || duo=='22' || duo=='24' || duo=='25' || duo=='32' || duo=='34' || duo=='35')
-				{coins+='3';}
-				else if(duo=='12' || duo=='14' || duo=='15' || duo=='42' || duo=='44' || duo=='45')
-				{coins+='4';}
+				// plat
+				var wTot = cnf.w + (cnf.sides[0] * p.set.espaisseur) + (cnf.sides[1] * p.set.espaisseur) + cnf.delta[0] + cnf.delta[1];
+				cmd.push(cld[0] +' '+ (mlt.x * wTot));
 			}
-
-			return coins;
+			else
+			{
+				// cranté
+				var crans = Math.floor(Math.abs(cnf.w)/p.set.wcrans);
+				if((cnf.type<=2 && p.isEven(crans)) || (cnf.type>=3 && p.isOdd(crans))) {crans++;}
+				wcrans = cnf.w/crans;
+	
+				for(i=1; i<=crans; i++)
+				{
+					//coté adjacent up
+					if(i==1)
+					{cmd.push(cld[0] +' '+ (mlt.x * (wcrans + (cnf.sides[0] * p.set.espaisseur) + cnf.delta[0])));}
+					else if(i==crans)
+					{cmd.push(cld[0] +' '+ (mlt.x * (wcrans + (cnf.sides[1] * p.set.espaisseur) + cnf.delta[1])));}
+					else
+					{
+						cmd.push(cld[0] +' '+ (mlt.x * wcrans));
+					}
+			
+					if(i<crans)
+					{
+						cmd.push(cld[1] +' '+ (mlt.y * dir * p.set.espaisseur));
+						dir*=-1;
+					}
+				}
+			}
+			
+			return cmd.join(' ');
 		}
 
-		p.refreshElements = function(el)
+		/**********************************************************/
+		/*									 ACTIONS BOUTONS											*/
+		/**********************************************************/
+		p.uploadConf = function()
 		{
-			var elmID = el.attr('id');
-			p.set.listElms[elmID].cotes = el.find('.h').attr('data-type')+el.find('.d').attr('data-type')+el.find('.b').attr('data-type')+el.find('.g').attr('data-type');
-			p.set.listElms[elmID].coins = p.recreerCoins(p.set.listElms[elmID].cotes);
-			el.find('.hg').attr('data-type', coins[0]);
-			el.find('.hd').attr('data-type', coins[1]);
-			el.find('.bd').attr('data-type', coins[2]);
-			el.find('.bg').attr('data-type', coins[3]);
-
-			p.updateSVGView();
+			console.log('Charger une configuration');
+			return false;
 		}
-		/*
-		p.transmuterCoin = function(e)
+		p.downloadConf = function()
 		{
-			tp = parseInt($(e.currentTarget).attr('data-type'));
-			$(e.currentTarget).attr('data-type', (tp>=4) ? 1 : tp+1);
-		}
-		*/
-		p.transmuterCote = function(e)
-		{
-			tp = parseInt($(e.currentTarget).attr('data-type'));
-			$(e.currentTarget).attr('data-type', (tp>=5) ? 1 : tp+1);
-			p.refreshElements($(e.currentTarget).parents('.element'));
+			console.log('Télécharger une configuration');
+			return false;
 		}
 
+		p.downloadSVG = function()
+		{
+			console.log('Téléchaerger le SVG');
+			var element = document.createElement('a');
+			text = document.querySelector('svg').outerHTML;
+			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + text);
+			element.setAttribute('download', 'plan.svg');
+		
+			element.style.display = 'none';
+			document.body.appendChild(element);
+		
+			element.click();
+		
+			document.body.removeChild(element);
+			return false;
+		}
 		p.killMe = function(e)
 		{
 			var elmID = $(e.currentTarget).parents('.element').attr('id');
@@ -250,37 +226,10 @@
 			return false;
 		}
 
+
 		/**********************************************************/
-		/*												 SETUP 													*/
+		/*										 INTERFACE													*/
 		/**********************************************************/
-		p.bazDataChanged = function(e)
-		{
-			var fld = $(e.currentTarget);
-			var k = (fld.attr('id').includes('.')) ? fld.attr('id').split('.') : fld.attr('id');
-			var newV = (isNaN(fld.val())) ? fld.val() : parseFloat(fld.val());
-			if(Array.isArray(k))
-			{p.set[k[0]][k[1]] = newV;}
-			else
-			{p.set[k] = newV;}
-
-			p.updateSVGView();
-		}
-
-		p.elmDataChanged = function(e)
-		{
-			var fld = $(e.currentTarget);
-			var elmID = fld.parents('.element').attr('id');
-			var k = (fld.attr('id').includes('.')) ? fld.attr('id').split('.') : fld.attr('id');
-			var newV = (isNaN(fld.val())) ? fld.val() : parseFloat(fld.val());
-			if(Array.isArray(k))
-			{p.set.listElms[elmID][k[0]][k[1]] = newV;}
-			else
-			{p.set.listElms[elmID][k] = newV;}
-
-			p.updateSVGView();
-		}
-
-
 		/**
 		 * Création de l'interface de l'application
 		 */
@@ -343,79 +292,41 @@
 			cnf.appendTo($elm);
 		}
 
-		p.creerActions = function()
+		p.creerElement = function(req)
 		{
-			p.set.addOne.click(p.addOne);
-			p.set.btnLoad.click(p.uploadConf);
-			p.set.btnSave.click(p.downloadConf);
-			p.set.btnDown.click(p.downloadSVG);
-			p.set.zonechps.find('input').on('change', p.updateSVGView);
-
-			//p.set.zoneElms.on('click', '.coin', p.transmuterCoin);
-			p.set.zoneElms.on('click', '.cote', p.transmuterCote);
-			p.set.zoneElms.on('change', 'input', p.elmDataChanged);
-			p.set.zoneBase.on('change', 'input', p.bazDataChanged);
-		}
-
-		
-		/**********************************************************/
-		/*												 UTILS 													*/
-		/**********************************************************/
-
-		p.creerLigneDecoupe = function(req)
-		{
-			def = {w: 50, sens: 0, type:1, delta: 0, sides: [0,0]};
-			var cnf = $.extend({}, def, req);
-			//console.log(cnf);
-			cmd = [];
-
-			// sélection de la progression de découpe : horizontal/vertical, xy/yx et direction
-			cld = p.isEven(cnf.sens) ? 'hv' : 'vh';
-			dir = (p.isEven(cnf.type)) ? -1 : 1;
-			mlt = {
-				x: (cnf.sens<2) ? 1 : -1,
-				y: (cnf.sens==1 || cnf.sens==2) ? -1 : 1
+			var def = {
+				'type': 'quad', 'x' : 0, 'y' : 0, 'w' : 75, 'h' : 40, 
+				'cotes':	"1111", 'dlta': [0,0,0,0], "id" : 'elm'+p.uid(), 'kp':[0,0,0,0,0,0,0,0]
 			};
+			var blockConf = $.extend({}, def, req);
+			blockConf.coins = p.recreerCoins(blockConf.cotes);
+			var zconf = $('<div class="zconf"></div>');
+			var btnlst = $('<div class="btnlst"></div>').appendTo(zconf);
+			$('<a href="#">X</a>').click(p.killMe).appendTo(btnlst);
+			$('<a href="#">+1</a>').click(p.copyMe).appendTo(btnlst);
+
+			p.creerFieldSet('base',JSON.parse('[["w","Larg.", '+blockConf.w+'],["h","Haut.", '+blockConf.h+'],["x","X", '+blockConf.x+'],["y","Y", '+blockConf.y+']]')).addClass('quadri').appendTo(zconf);
+			p.creerFieldSet('dlta',JSON.parse('[["dlta.0","⮝", '+blockConf.dlta[0]+'],["dlta.1","⮞", '+blockConf.dlta[1]+'],["dlta.2","⮟", '+blockConf.dlta[2]+'],["dlta.3","⮜", '+blockConf.dlta[3]+']]')).addClass('quadri').prepend('<strong>Décalage</strong>').appendTo(zconf);
+			p.creerFieldSet('dkp1',JSON.parse('[["kp.0","⮝", '+blockConf.kp[0]+'],["kp.1","⮞", '+blockConf.kp[1]+'],["kp.2","⮟", '+blockConf.kp[2]+'],["kp.3","⮜", '+blockConf.kp[3]+'],["kp.4","⮝", '+blockConf.kp[0]+'],["kp.5","⮞", '+blockConf.kp[1]+'],["kp.6","⮟", '+blockConf.kp[2]+'],["kp.7","⮜", '+blockConf.kp[3]+']]')).addClass('quadri').prepend('<strong>Découpe delta / arrondi</strong>').appendTo(zconf);
 
 
-			if(cnf.type=="5")
-			{
-				// plat
-				var wTot = cnf.w + (cnf.sides[0] * p.set.espaisseur) + (cnf.sides[1] * p.set.espaisseur) + cnf.delta[0] + cnf.delta[1];
-				cmd.push(cld[0] +' '+ (mlt.x * wTot));
-			}
-			else
-			{
-				// cranté
-				var crans = Math.floor(Math.abs(cnf.w)/p.set.wcrans);
-				if((cnf.type<=2 && p.isEven(crans)) || (cnf.type>=3 && p.isOdd(crans))) {crans++;}
-				wcrans = cnf.w/crans;
-	
-				for(i=1; i<=crans; i++)
-				{
-					//coté adjacent up
-					if(i==1)
-					{cmd.push(cld[0] +' '+ (mlt.x * (wcrans + (cnf.sides[0] * p.set.espaisseur) + cnf.delta[0])));}
-					else if(i==crans)
-					{cmd.push(cld[0] +' '+ (mlt.x * (wcrans + (cnf.sides[1] * p.set.espaisseur) + cnf.delta[1])));}
-					else
-					{
-						cmd.push(cld[0] +' '+ (mlt.x * wcrans));
-					}
+			var configurateur = $('<div class="element" data-cotes="'+blockConf.cotes+'" id="'+blockConf.id+'"></div>');
+			$('<div data-type="'+blockConf.coins[0]+'" class="coin hg"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.cotes[0]+'" class="cote h"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.coins[1]+'" class="coin hd"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.cotes[3]+'" class="cote g"><span></span></div>').appendTo(configurateur);
+			zconf.appendTo(configurateur);
+			$('<div data-type="'+blockConf.cotes[1]+'" class="cote d"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.coins[3]+'" class="coin bg"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.cotes[2]+'" class="cote b"><span></span></div>').appendTo(configurateur);
+			$('<div data-type="'+blockConf.coins[2]+'" class="coin bd"><span></span></div>').appendTo(configurateur);
 			
-					if(i<crans)
-					{
-						cmd.push(cld[1] +' '+ (mlt.y * dir * p.set.espaisseur));
-						dir*=-1;
-					}
-				}
-			}
-			
-			return cmd.join(' ');
+			p.set.listElms[blockConf.id] = blockConf;
+			p.set.zoneElms.append(configurateur);
+
+			p.updateSVGView();
 		}
 
-		p.isEven = function(n) {return n % 2 == 0;}
-		p.isOdd = function(n) {return Math.abs(n % 2) == 1;}
 
 		/**
 		 * Crée un <div> avec la class k, contenant les champs définits dans l
@@ -443,10 +354,97 @@
 			return grp;
 		}
 
-		p.uid = function()
+		p.creerActions = function()
 		{
-			return Math.random().toString(16).slice(2).slice(-8)+'-'+Math.random().toString(16).slice(2).slice(-4);
+			p.set.addOne.click(p.addOne);
+			p.set.btnLoad.click(p.uploadConf);
+			p.set.btnSave.click(p.downloadConf);
+			p.set.btnDown.click(p.downloadSVG);
+			p.set.zonechps.find('input').on('change', p.updateSVGView);
+
+			//p.set.zoneElms.on('click', '.coin', p.transmuterCoin);
+			p.set.zoneElms.on('click', '.cote', p.transmuterCote);
+			p.set.zoneElms.on('change', 'input', p.elmDataChanged);
+			p.set.zoneBase.on('change', 'input', p.bazDataChanged);
 		}
+
+		/**********************************************************/
+		/*											 UPDATERS													*/
+		/**********************************************************/
+		p.recreerCoins = function(c)
+		{
+			coins = "";
+
+			for(i=0; i<=3; i++)
+			{
+				duo = ((i==0)?c[3]:c[i-1])+''+c[i]; //création d'une chaine des cote précédents et suivants
+				if(duo=='11' || duo=='13' || duo=='41' || duo=='43')
+				{coins+='1';}
+				else if(duo=='51' || duo=='53' || duo=='21' || duo=='23' || duo=='31' || duo=='33')
+				{coins+='2';}
+				else if(duo=='52' || duo=='54' || duo=='55' || duo=='22' || duo=='24' || duo=='25' || duo=='32' || duo=='34' || duo=='35')
+				{coins+='3';}
+				else if(duo=='12' || duo=='14' || duo=='15' || duo=='42' || duo=='44' || duo=='45')
+				{coins+='4';}
+			}
+
+			return coins;
+		}
+
+		p.refreshElements = function(el)
+		{
+			var elmID = el.attr('id');
+			p.set.listElms[elmID].cotes = el.find('.h').attr('data-type')+el.find('.d').attr('data-type')+el.find('.b').attr('data-type')+el.find('.g').attr('data-type');
+			p.set.listElms[elmID].coins = p.recreerCoins(p.set.listElms[elmID].cotes);
+			el.find('.hg').attr('data-type', coins[0]);
+			el.find('.hd').attr('data-type', coins[1]);
+			el.find('.bd').attr('data-type', coins[2]);
+			el.find('.bg').attr('data-type', coins[3]);
+
+			p.updateSVGView();
+		}
+
+		p.transmuterCote = function(e)
+		{
+			tp = parseInt($(e.currentTarget).attr('data-type'));
+			$(e.currentTarget).attr('data-type', (tp>=5) ? 1 : tp+1);
+			p.refreshElements($(e.currentTarget).parents('.element'));
+		}
+
+		p.bazDataChanged = function(e)
+		{
+			var fld = $(e.currentTarget);
+			var k = (fld.attr('id').includes('.')) ? fld.attr('id').split('.') : fld.attr('id');
+			var newV = (isNaN(fld.val())) ? fld.val() : parseFloat(fld.val());
+			if(Array.isArray(k))
+			{p.set[k[0]][k[1]] = newV;}
+			else
+			{p.set[k] = newV;}
+
+			p.updateSVGView();
+		}
+
+		p.elmDataChanged = function(e)
+		{
+			var fld = $(e.currentTarget);
+			var elmID = fld.parents('.element').attr('id');
+			var k = (fld.attr('id').includes('.')) ? fld.attr('id').split('.') : fld.attr('id');
+			var newV = (isNaN(fld.val())) ? fld.val() : parseFloat(fld.val());
+			if(Array.isArray(k))
+			{p.set.listElms[elmID][k[0]][k[1]] = newV;}
+			else
+			{p.set.listElms[elmID][k] = newV;}
+
+			p.updateSVGView();
+		}
+
+
+		/**********************************************************/
+		/*												 UTILS 													*/
+		/**********************************************************/
+		p.isEven = function(n) {return n % 2 == 0;}
+		p.isOdd = function(n) {return Math.abs(n % 2) == 1;}
+		p.uid = function() {return Math.random().toString(16).slice(2).slice(-8)+'-'+Math.random().toString(16).slice(2).slice(-4);}
 		
 
 		p.init();
